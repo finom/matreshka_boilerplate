afab8a9988ba240ba2ae5e47b8b07453b6eb7f04
Andrey Gubanov <bookfast.org@gmail.com>
2015-10-12 15:52:10 +0300

    minor

--- src/config.precompiled-example.js
+++ src/config.precompiled-example.js
@@ -2,10 +2,7 @@ System.config({
     baseURL: '.',
     defaultJSExtensions: true,
     map: {
-        matreshka_dir: 'js/lib/matreshka',
-        matreshka: 'js/lib/matreshka/amd-modules/matreshka',
-        balalaika: 'js/lib/matreshka/amd-modules/balalaika',
-        xclass: 'js/lib/matreshka/amd-modules/xclass',
+        matreshka: 'js/lib/matreshka',
         text: 'js/lib/text',
         lib: 'js/lib',
         app: 'js/app'
